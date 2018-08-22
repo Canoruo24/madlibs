78860faed335627e904e422305b9173eafd0e5bc
Christopher Anoruo <quest4chris2004@gmail.com>
2018-08-22 16:39:06 -0700

    styled the card Component

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Header = (title) => {
     return (
         <div className="home">
+        <br/>
         <h1>Bottega Madlibs</h1>
         <br/>
         <p>Fill out the filds below and click 
